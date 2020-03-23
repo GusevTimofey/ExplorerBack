906b1cb81d2c6bd48d578a38d104f5a867c17706
Timofey Gusev <gusev.t.s1@gmail.com>
2020-03-23 19:09:34 +0300

    more additions to the ChainSynchronizer program

--- src/main/scala/encry/explorer/chain/observer/programms/ChainSynchronizer.scala
+++ src/main/scala/encry/explorer/chain/observer/programms/ChainSynchronizer.scala
@@ -1,11 +1,30 @@
 package encry.explorer.chain.observer.programms
 
+import cats.effect.Sync
+import cats.syntax.flatMap._
+import cats.syntax.functor._
+import encry.explorer.chain.observer.services.NodeObserver
+import encry.explorer.core.{ HeaderHeight, Id, UrlAddress }
+import eu.timepit.refined.types.string.HexString
 import fs2.Stream
+import org.http4s.client.Client
 
 trait ChainSynchronizer[F[_]] {
   def run: Stream[F, Unit]
 }
 
 object ChainSynchronizer {
+
+  def apply[F[_]: Sync](client: Client[F]): ChainSynchronizer[F] = new ChainSynchronizer[F] {
+    override def run: Stream[F, Unit] = ???
+
+    val observerService: NodeObserver[F] = NodeObserver(client, UrlAddress("http://172.16.11.14:9051"))
+
+    def getActualInfo(initHeight: Int) =
+      for {
+        id    <- observerService.getBestBlockIdAt(HeaderHeight(initHeight))
+        block <- observerService.getBlockBy(Id(id.get))
+      } yield ()
+  }
 
 }
