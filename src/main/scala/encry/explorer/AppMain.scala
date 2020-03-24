968df4eb7af0a9d1df3d1a039f8878c35c3514af
Timofey Gusev <gusev.t.s1@gmail.com>
2020-03-24 14:32:29 +0300

    cleaned up code

--- src/main/scala/encry/explorer/AppMain.scala
+++ src/main/scala/encry/explorer/AppMain.scala
@@ -2,8 +2,7 @@ package encry.explorer
 
 import cats.effect.{ ExitCode, IO, IOApp }
 import cats.syntax.functor._
-import encry.explorer.chain.observer.programms.ChainSynchronizer
-import fs2.Stream
+import encry.explorer.chain.observer.programs.ChainSynchronizer
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 import org.http4s.client.blaze.BlazeClientBuilder
 
@@ -11,16 +10,15 @@ import scala.concurrent.ExecutionContext
 
 object AppMain extends IOApp {
   override def run(args: List[String]): IO[ExitCode] =
-    Slf4jLogger
-      .create[IO]
-      .flatMap { implicit log =>
-        Stream
-          .resource(BlazeClientBuilder[IO](ExecutionContext.global).resource)
-          .evalMap(client => ChainSynchronizer.apply[IO](client))
-          .flatMap(_.run)
-          .compile
-          .drain
-      }
-      .as(ExitCode.Success)
+    BlazeClientBuilder[IO](ExecutionContext.global).resource.use { client =>
+      Slf4jLogger
+        .create[IO]
+        .flatMap { implicit logging =>
+          ChainSynchronizer
+            .apply[IO](client)
+            .flatMap(_.run.compile.drain)
+        }
+        .as(ExitCode.Success)
+    }
 
 }
--- src/main/scala/encry/explorer/chain/observer/programs/ChainSynchronizer.scala
+++ src/main/scala/encry/explorer/chain/observer/programs/ChainSynchronizer.scala
@@ -1,4 +1,4 @@
-package encry.explorer.chain.observer.programms
+package encry.explorer.chain.observer.programs
 
 import cats.effect.{ Concurrent, ConcurrentEffect, Sync, Timer }
 import cats.syntax.applicative._
@@ -11,9 +11,7 @@ import fs2.Stream
 import fs2.concurrent.Queue
 import io.chrisdavenport.log4cats.Logger
 import org.http4s.client.Client
-import org.http4s.client.blaze.BlazeClientBuilder
 
-import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
 trait ChainSynchronizer[F[_]] {
@@ -28,35 +26,26 @@ object ChainSynchronizer {
     } yield
       new ChainSynchronizer[F] {
         override def run: Stream[F, Unit] =
-          Stream.eval(getActualInfo(0)) concurrently Stream
-            .emit(())
+          Stream(()).repeat
             .covary[F]
-            .metered(0.5.seconds)
-            .evalMap(_ => queue.dequeue1)
+            .metered(1.seconds)
+            .evalMap(_ => Logger[F].info("Repeat") >> queue.dequeue1.void) concurrently Stream.eval(getActualInfo(0))
 
         val observerService: F[NodeObserver[F]] =
           for {
-            _   <- Logger[F].info(s"Create observer service step 1")
             url <- UrlAddress.fromString[F]("http://172.16.11.14:9051")
-            _   <- Logger[F].info(s"Create observer service step 2")
             res <- NodeObserver(client, url).pure[F]
-            _   <- Logger[F].info(s"Create observer service step 3")
           } yield res
 
         def getActualInfo(initHeight: Int): F[Unit] =
-          for {
-            _       <- Logger[F].info(s"Run observer step 1")
+          (for {
             service <- observerService
-            _       <- Logger[F].info(s"Run observer step 2")
             idRaw   <- service.getBestBlockIdAt(HeaderHeight(initHeight))
-            _       <- Logger[F].info(s"Run observer step 3")
             id      <- Id.fromString(idRaw.get)
-            _       <- Logger[F].info(s"CRun observer step 4")
             block   <- service.getBlockBy(id)
-            _       <- Logger[F].info(s"Got $block from http api.")
             _       <- queue.enqueue1(block.get)
             _       <- Timer[F].sleep(1.seconds)
-          } yield getActualInfo(initHeight + 1)
+          } yield ()) >> getActualInfo(initHeight + 1)
 
       }
 
