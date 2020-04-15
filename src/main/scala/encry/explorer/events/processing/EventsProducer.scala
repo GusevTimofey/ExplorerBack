b546c6471f3c3fb489eed8e06b85d9df5175e0c7
Timofey Gusev <gusev.t.s1@gmail.com>
2020-04-15 12:20:59 +0300

    minor producer upgrade

--- src/main/scala/encry/explorer/events/processing/EventsProducer.scala
+++ src/main/scala/encry/explorer/events/processing/EventsProducer.scala
@@ -2,12 +2,13 @@ package encry.explorer.events.processing
 
 import cats.effect.{ ConcurrentEffect, ContextShift, Timer }
 import cats.syntax.functor._
-import cats.syntax.flatMap._
+import cats.syntax.traverse._
 import encry.explorer.core.settings.ExplorerSettings
-import fs2.Stream
+import fs2.{ Chunk, Stream }
 import fs2.concurrent.Queue
 import fs2.kafka._
 import io.chrisdavenport.log4cats.Logger
+
 import scala.concurrent.duration._
 
 trait EventsProducer[F[_]] {
@@ -26,21 +27,19 @@ object EventsProducer {
       valueSerializer = explorerEventKafkaSerializer.createSerializer[F]
     ).withBootstrapServers(ES.kafkaSettings.consumerAddress)
 
-    private def kafkaProducer: Stream[F, ProducerResult[String, ExplorerEvent, Unit]] =
+    private def kafkaProducer: Stream[F, Chunk[ProducerResult[String, ExplorerEvent, Unit]]] =
       producerStream(producerSettings).flatMap { producer =>
-        getNextEvent
+        eventsQueue.dequeue
           .evalTap(event => Logger[F].info(s"Going to produce new explorer event: $event."))
           .map(newEvent => ProducerRecords.one(ProducerRecord(newEvent.kafkaTopic, newEvent.kafkaKey, newEvent)))
           .evalTap(_ => Logger[F].info(s"New event created"))
-          .through(produce(producerSettings, producer))
+          .evalMap(producer.produce)
+          .groupWithin(20, 5.seconds)
+          .evalMap(_.sequence)
           .evalTap(_ => Logger[F].info(s"New explorer event produced."))
       }.handleErrorWith { err =>
         Stream.eval(Logger[F].info(s"Error ${err.getMessage} has occurred while processing kafka producer")) >>
           kafkaProducer
       }
-
-    private def getNextEvent: Stream[F, ExplorerEvent] =
-      Stream(()).covary[F].repeat.metered(0.1.seconds).evalMap(_ => eventsQueue.dequeue1)
-
   }
 }
